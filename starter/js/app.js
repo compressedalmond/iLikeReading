36e06e8571993e8ac9aa03ecd193e153abe700db
compressedalmond <elanh0412@gmail.com>
2025-07-27 16:27:32 -0500

    13.1

--- starter/js/app.js
+++ starter/js/app.js
@@ -372,9 +372,20 @@ const books = [
 ]
 
 
-function toggleMode() {
-  document.body.classList.toggle('dark');
-}
+  window.onload = function () {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark');
+    }
+  }
+
+  function toggleMode() {
+    document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+  }
 
 if (window.location.href.includes("index")){
   const homeBooks = document.getElementById("homeBooks");
